d06d0b0e56937fd71a913e23810795db83edb456
lemonpie313 <156773875+lemonpie313@users.noreply.github.com>
2024-05-02 13:29:15 +0900

    [level 1] Title: 시저 암호, Time: 1.82 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,17 +1,14 @@
-function solution(s,n) {
-    
-    s = s.split("").map((cur) => {
-        let index = cur.charCodeAt();
-        if (index>=65 && index <=90) {
-            index=(index+n<=90)?(index+n):(index+n-90+64);
-            return String.fromCharCode(index);
-        } else if (index>=97 && index<=122) {
-            index=(index+n<=122)?(index+n):(index+n-122+96);
-            return String.fromCharCode(index);
-        } else {
-            return String.fromCharCode(index);
-        }
-    }).join("");
+function solution(s, n) {
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lower = "abcdefghijklmnopqrstuvwxyz";
 
-    return s;
+    return s.split("").map((cur) => {
+        if (cur == " ") {
+            return " ";
+        }
+        var textArr = upper.includes(cur) ? upper : lower;
+        var index = (textArr.indexOf(cur) + n) % textArr.length;
+        return textArr[index];
+
+    }).join("");
 }
