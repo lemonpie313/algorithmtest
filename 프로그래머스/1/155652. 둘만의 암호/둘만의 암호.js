ec2a977bbef8e8d3b52fa9d7191072b0086a0d7a
lemonpie313 <156773875+lemonpie313@users.noreply.github.com>
2024-05-30 15:02:22 +0900

    [level 1] Title: 둘만의 암호, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -2,10 +2,10 @@ function solution(s, skip, index) {
   let alphabet = 'abcdefghijklmnopqrstuvwxyz';
   let replacedAlphabet = skip.split("").reduce((acc, cur)=> {
     return acc.replace(cur, "");
-  }, alphabet).repeat(5).split("");
-
+  }, alphabet).split("");
   let ans = s.split("").map((cur) => {
-    return replacedAlphabet[replacedAlphabet.indexOf(cur)+index];
+    const idx = (replacedAlphabet.indexOf(cur)+index)%replacedAlphabet.length;
+    return replacedAlphabet[idx];
   }).join("");
   return ans;
 }
