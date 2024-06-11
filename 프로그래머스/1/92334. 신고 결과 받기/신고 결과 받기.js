7688562453559190855b916b143979edaf54220a
lemonpie313 <156773875+lemonpie313@users.noreply.github.com>
2024-06-11 20:33:29 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -3,11 +3,11 @@ function solution(id_list, report, k) {
   ans.fill(0) 
   const report_list = {}
     
-  id_list.map((user)=>{
-      report_list[user] = [] //key로 userid를 value로 빈 배열을 가지는 객체
+  id_list.forEach((user)=>{
+      report_list[user] = []
   })
   
-  report.map((user)=>{
+  report.forEach((user)=>{
       const [user_id, report_id] = user.split(' ')
       if(!report_list[report_id].includes(user_id)){
           report_list[report_id].push(user_id)
@@ -15,8 +15,8 @@ function solution(id_list, report, k) {
   })
   
   for(const key in report_list){
-      if(report_list[key].length >= k){ //이용정지 유저
-          report_list[key].map((user)=>{
+      if(report_list[key].length >= k){
+          report_list[key].forEach((user)=>{
               ans[id_list.indexOf(user)] += 1
           })
       }
