cc6cdd6a2596e940a46b12d9cb3e8c958727f4ee
Jin Suhyeon <156773875+lemonpie313@users.noreply.github.com>
2024-07-11 10:10:58 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 71.04 ms, Memory: 149 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,29 @@
+function solution(numbers) {
+    var answer = [];
+    let maxnum = 0;
+    let stack = [];
+    for(let i=numbers.length-1; i>=0; i--){
+        if(maxnum<=numbers[i]){
+            maxnum = numbers[i];
+            answer.push(-1);
+            stack = [];
+            stack.push(numbers[i]);
+        }else{
+            let j=0;
+            while(1){
+              if(numbers[i]<stack[j]){
+                 answer.push(stack[j]);
+                 stack.unshift(numbers[i]); 
+                 break;
+               }else{
+                   stack.shift();
+               }
+            } 
+        }
+    }
+
+
+    answer.reverse();
+
+    return answer;
+}
