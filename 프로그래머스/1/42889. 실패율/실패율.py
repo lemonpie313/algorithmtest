4fa7a97fad39087aaf4f2e3522767450ed645fcd
Jin Suhyeon <156773875+lemonpie313@users.noreply.github.com>
2025-10-05 15:57:32 +0900

    [level 1] Title: 실패율, Time: 5351.53 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,10 +1,13 @@
 def solution(N, stages):
-    fail = {}
-    for i in range(1,N+1):
-        try:
-            fail_ = len([a for a in stages if a==i])/len([a for a in stages if a>=i])
-        except:
-            fail_ = 0
-        fail[i]=fail_
-    answer = sorted(fail, key=fail.get, reverse=True)
-    return answer
+    failureresult = {}
+    for i in range(1, N+1):
+        still = stages.count(i)
+        now = [n for n in stages if n>=i]
+        if len(now)==0:
+            failure = 0
+        else:
+            failure = still/len(now)
+        failureresult[i] = failure
+    
+    failures = sorted(failureresult, key=lambda x: failureresult[x], reverse = True)
+    return failures
