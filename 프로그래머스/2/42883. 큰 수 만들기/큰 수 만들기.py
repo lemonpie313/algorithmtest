4759e525439a0d92b184481ae57f194fe600a723
Jin Suhyeon <156773875+lemonpie313@users.noreply.github.com>
2025-10-08 01:09:54 +0900

    [level 2] Title: 큰 수 만들기, Time: 129.52 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,16 +1,11 @@
 def solution(number, k):
-    
-    start = 0
-    for i in range(k):
-        remover = 0
-        for j in range(start, len(number)-1):
-            if number[j]<number[j+1]:
-                start = max(j-1, 0)
-                remover = number[j]
-                break
-            if j ==len(number)-2:
-                return number[i:-1]
-        number = number.replace(str(remover), '', 1)
-                
-        
-    return number
+    array = [number[0]]
+
+    for i in number[1:]:
+        while (len(array) != 0) and (array[-1] < i) and (k != 0):
+            k -= 1
+            array.pop()
+        array.append(i)
+    if k != 0:
+        array = array[:-k]
+    return "".join(array)
