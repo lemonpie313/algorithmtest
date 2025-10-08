b7931ddf43fcff4b1a583055b43bf3d785f854a0
Jin Suhyeon <156773875+lemonpie313@users.noreply.github.com>
2025-10-08 17:03:13 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,22 @@
+def solution(name):
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alp = {}
+    for i in range(len(alphabet)):
+        alp[alphabet[i]] = i
+
+    spell_move = 0
+    cursor_move = len(name) - 1
+
+    for i, spell in enumerate(name):
+        spell_move += min(ord(spell) - ord('A'), ord('Z') - ord(spell) + 1)
+
+        next = i+1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+
+        cursor_move = min([cursor_move, 2*i+len(name)-next, i+2*(len(name)-next)])
+
+    return spell_move + cursor_move
+
+
+# [A]AA -> [J]AA -> JA[] -> JAZ
